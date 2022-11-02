256bdc5c2054e26e655d7bfdb25cdc9efd521fea
Braden James <97642471+bradenbjames@users.noreply.github.com>
2022-11-02 13:12:31 -0500

    Update Fruit.java

--- src/Fruit.java
+++ src/Fruit.java
@@ -4,11 +4,4 @@ public class Fruit extends cs331Fruit {
     public Fruit() {
 
     }
-
-    public void makeApple() {
-        this.ripen();
-        this.wash();
-        this.moveFruit();
-    }
-
 }
