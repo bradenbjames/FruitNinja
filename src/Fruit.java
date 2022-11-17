2d70025e00c2aadc2140b2a935e72dbf4b5afbf8
singhd1 <singhd1@sfasu.edu>
2022-11-16 18:23:55 -0600

    my feedback

--- src/Fruit.java
+++ src/Fruit.java
@@ -1,7 +1,20 @@
 import fruitNinjaHelper.cs331Fruit;
 
-public class Fruit extends cs331Fruit {
+// THIS CLASS SHOULD ALSO IMPLEMENT ICHOPPABLE INTERFACE. BUT IT WILL NOT EXPLODE BUT CHOPGRAPHICALLY, WHERE AS BOMB CLASS WILL IMPLEMENT THE METHOD TO EXPLODE. 
+public class Fruit extends cs331Fruit implement IChoppable {
     public Fruit() {
 
     }
+
+    public prepare() {
+	wash();
+	ripen();
+    }
+
+    // this method should be declared in the IChoppable interface
+    @Override
+    public doSomething() {
+	
+    }
+
 }
--- src/Lemon.java
+++ src/Lemon.java
@@ -4,6 +4,7 @@ import fruitNinjaHelper.cs331Fruit;
 public class Lemon extends cs331Fruit implements IChoppable {
     private int pointValue = 2;
 
+    // YOU HAVE DUPLICATE CODE IN ALL THE FRUIT SUBCLASSES. YOU NEED TO PUT THIS IN FRUIT CLASS AND CHILD CLASS WILL INHERIT IT AUTOMATICALLY
     public Lemon() {
         this.setImage(Constants.LEMON_PATH);
         this.ripen();
